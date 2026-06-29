689789e3625931ce45a4fe19eb5af41d61bbb3db
alex <thealbertmak@gmail.com>
2026-06-29 19:03:35 +0800

    mandoc instead of man-db

--- bin/packages.sh
+++ bin/packages.sh
@@ -4,7 +4,7 @@ set -e
 sudo pacman -Syu --noconfirm
 
 sudo pacman -S --needed --noconfirm base-devel curl git zip unzip trash-cli tree
-sudo pacman -S --needed --noconfirm zsh fzf ripgrep fd the_silver_searcher fastfetch tldr
+sudo pacman -S --needed --noconfirm zsh fzf ripgrep fd the_silver_searcher fastfetch mandoc
 sudo pacman -S --needed --noconfirm neovim yazi tmux lazygit git-delta
 sudo pacman -S --needed --noconfirm htop cmake make ninja gdb clang llvm lldb bear podman
 
