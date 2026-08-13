bd67e9521b24df42f6573a2b386e78637e980b9b
Alex Mak <aliasgmail@duck.com>
2026-08-13 16:22:04 +0800

    optimized startup time

--- nvim/plugin/statusline.lua
+++ nvim/plugin/statusline.lua
@@ -17,6 +17,13 @@ local augroup = vim.api.nvim_create_augroup("CustomStatusline", { clear = true }
 vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
 	group = augroup,
 	callback = function()
+		-- don't show statusline on float window
+		local win_config = vim.api.nvim_win_get_config(0)
+		if win_config.relative ~= "" or vim.bo.buftype == "terminal" or vim.bo.buftype == "nofile" then
+			vim.opt_local.statusline = ""
+			return
+		end
+
 		vim.opt_local.statusline = table.concat({
 			" ",
 			"%{v:lua.git_branch()}",
