4c69dbc07c9f4c4bbf73a9878525b4ff762e5d40
Alex Mak <113433667+victoryfeel@users.noreply.github.com>
2026-09-02 00:34:45 +0800

    fixed: configsys git
    1. configsys.sh: fix user name as "Alex Mak".

--- bin/configsys.sh
+++ bin/configsys.sh
@@ -10,7 +10,7 @@ mkdir -p "$HOME/alexmak/learn"
 mkdir -p "$HOME/alexmak/ongo"
 mkdir -p "$HOME/tmp"
 
-git config --global user.name "alex"
+git config --global user.name "Alex Mak"
 git config --global user.email "113433667+victoryfeel@users.noreply.github.com"
 git config --global init.defaultBranch main
 # systemctl --user enable --now podman.socket
